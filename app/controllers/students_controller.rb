7b50326070bb33c4cb67cbd08b3df06c41700d7f
Nathan Brickett <natey37@gmail.com>
2020-02-01 12:47:48 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,14 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new 
+    
   end
 
   def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+
+    redirect_to student_path(@student)
   end
 
 end
